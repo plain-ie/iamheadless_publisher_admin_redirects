f6957dda5ab7db4f6fa082198690bdeb769609f6
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-04-04 08:52:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
 
     # Dependent packages (distributions)
     install_requires=[
-        "django>=4.0.2",
+        # 'https://github.com/plain-ie/iamheadless_publisher_admin/archive/refs/heads/main.zip',
     ],
 
 )
